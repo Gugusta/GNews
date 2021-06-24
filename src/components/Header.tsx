6b8b803df484e4ea6d2e6e314c8a375450a13cac
Gustavo Serão <gustavo.serao@outlook.com>
2021-06-24 20:44:14 -0300

    feat: Header Created

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import styles from "./styles.module.scss";
+
+import logo from "../../public/images/logo.svg";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src={logo} alt="Logo IG News" />
+
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
