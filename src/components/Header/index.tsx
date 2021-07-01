19a6d48a81339394af797dff76aed6783af7dabf
Gustavo Serão <gustavo.serao@outlook.com>
2021-07-01 09:33:41 -0300

    feat: API Integration

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import styles from "./styles.module.scss";
+import { SignInButton } from "../SignInButton";
 
-import logo from "../../public/images/logo.svg";
+import logo from "../../../public/images/logo.svg";
 
 export function Header() {
   return (
@@ -13,6 +14,8 @@ export function Header() {
           <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,57 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
+import { GetStaticProps } from "next";
+import { SubscribeButton } from "../components/SubscribeButton";
+import styles from "./home.module.scss";
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
-        <title> Início | G News </title>
+        <title> Home | G News </title>
       </Head>
-      <h1>Hello Word</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span> 👏 Oi, seja bem vindo!</span>
+          <h1>
+            Tudo sobre o nosso <span>Mundo</span>
+          </h1>
+          <p>
+            Tenha acesso a todas as publicações <br />
+            <span> por {product.amount}/mês</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Codding" />
+      </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1J8BT5Hx7RDh6ETMQ098Ykod");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(price.unit_amount / 100),
+  };
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
